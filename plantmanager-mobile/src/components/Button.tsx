d4774d67d2ef1b16ebb0cb6e79e8d5d0cd340e97
ItaloPussi <italopussi@gmail.com>
2021-04-21 00:05:13 -0300

    NLW React Native #2: Icons, fonts, UserIdentification / Confirmation screens and navigation

--- plantmanager-mobile/src/components/Button.tsx
+++ plantmanager-mobile/src/components/Button.tsx
@@ -1,16 +1,26 @@
+import { useNavigation } from '@react-navigation/core'
 import React from 'react'
 import {TouchableOpacity, Text, StyleSheet, TouchableOpacityProps} from 'react-native'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 interface ButtonProps extends TouchableOpacityProps {
-    title: String;    
+    title: string;   
+    to: string; 
 }
 
-export function Button({title, ...rest} : ButtonProps){
+export function Button({title, to, ...rest} : ButtonProps){
+    
+    const navigation = useNavigation()
+
+    function handleNavigation(){
+        navigation.navigate(to)
+    }
     return (
         <TouchableOpacity 
             style={styles.button}
             activeOpacity={0.7}
+            onPress={handleNavigation}
             {...rest}
         >
             <Text style={styles.buttonText}>
@@ -26,12 +36,11 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 16,
-        marginBottom: 10,
         height: 56,
-        width: 56
     },
     buttonText: {
         color: colors.white,
-        fontSize: 24
+        fontSize: 16,
+        fontFamily: fonts.heading
     },
 })
---
+++ plantmanager-mobile/src/pages/UserIdentification.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import { SafeAreaView, View, StyleSheet, Text, TextInput, Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from 'react-native'
+import { Button } from '../components/Button'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function UserIdentification(){
+    const [name, setName] = useState<string>("")
+    const [isFocused, setIsFocused] = useState(false)
+    
+    function handleInputBlur(){
+        setIsFocused(false)
+    }
+
+    function handleInputFocus(){
+        setIsFocused(true)
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView 
+                style={styles.container}
+                behavior={Platform.OS === "ios" ? "padding" : 'height'}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <Text style={styles.emoji}>
+                                {
+                                    name.length >= 3 ? "😃" : "😄"
+                                }              
+                                
+                            </Text>
+
+                            <Text style={styles.title}>
+                                Como podemos {"\n"}
+                                chamar você?
+                            </Text>
+
+                            <TextInput
+                                value={name}
+                                onChangeText={setName}
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                style={[
+                                    styles.input,
+                                    (isFocused || name.length >= 3) && { borderColor: colors.green}
+                                ]}
+                                placeholder="Digite um nome"
+                            />
+
+                            <View style={styles.footer}>
+                                <Button to="Confirmation" title="Confirmar" />
+                            </View>
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: "center",
+        justifyContent: "space-around"
+    },
+    content: {
+        flex: 1,
+        width: "100%",
+    },
+    form: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 54,
+        alignItems: "center",
+    },
+    emoji: {
+        fontSize: 44
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: "100%",
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        textAlign: 'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        marginTop: 20
+    },
+    footer: {
+        marginTop: 40,
+        width: "100%",
+        paddingHorizontal: 20
+    }
+})
